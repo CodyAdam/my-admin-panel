b8b304bf3b13e7acd71aba81d6b28d9bce827604
Fgdou <fabigoardou@gmail.com>
2022-10-18 14:41:35 +0200

    ADD user object

--- fr-administration/src/users/users.controller.ts
+++ fr-administration/src/users/users.controller.ts
@@ -1,9 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { User } from './user.entity';
+
+const users: User[] = [
+    {
+        id: 0,
+        lastname: 'Doe',
+        firstname: 'John'
+    }
+]
 
 @Controller('users')
 export class UsersController {
     @Get('all')
-    getAll(): string[] {
-        return ['a', 'b', 'c', 'd']
+    getAll(): User[] {
+        return users
     }
 }
