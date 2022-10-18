bcf5f8bf8c020a5d34fc418881cc5e19a8eaacaf
Fgdou <fabigoardou@gmail.com>
2022-10-18 14:35:52 +0200

    delete funcky stuff

--- fr-administration/src/users/users.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('users')
-export class UsersController {
-    @Get('all')
-    getAll(): string[] {
-        return ['a', 'b', 'c', 'd']
-    }
-}
