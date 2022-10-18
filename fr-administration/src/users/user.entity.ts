acd1f1ce1e4a98e8febcf3237927e70799798ef7
Fgdou <fabigoardou@gmail.com>
2022-10-18 15:16:06 +0200

    ADD create and get to users controller

--- fr-administration/src/users/user.entity.ts
+++ fr-administration/src/users/user.entity.ts
@@ -2,4 +2,10 @@ export class User {
     id: number
     lastname: string
     firstname: string
+
+    constructor(id: number, lastname: string, firstname: string){
+        this.id = id
+        this.lastname = lastname
+        this.firstname = firstname
+    }
 }
--- fr-administration/src/users/users.controller.ts
+++ fr-administration/src/users/users.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import supertest from 'supertest';
 import { User } from './user.entity';
 
 const users: User[] = [
@@ -9,10 +10,31 @@ const users: User[] = [
     }
 ]
 
+function getNewId(): number {
+    let max = 0;
+    users.forEach(u => {
+        if(u.id > max)
+            max = u.id
+    });
+    return max+1
+}
+
 @Controller('users')
 export class UsersController {
-    @Get('all')
+    @Get()
     getAll(): User[] {
         return users
+    }
+
+    @Post()
+    create(@Body() input: any): User {
+        let user = new User(getNewId(), input.lastname, input.firstname)
+        users.push(user)
+        return user
+    }
+    
+    @Get(":id")
+    getUser(@Param('id', ParseIntPipe) id: number): User {
+        return users.find(u => u.id === id)
     }
 }
