6297635bbbc48ea874252ffa9fafe5c04408fbf8
fgdou <fabigoardou@gmail.com>
2022-10-18 17:06:28 +0200

    ADD assoc getMembers

--- fr-administration/src/associations/associations.controller.ts
+++ fr-administration/src/associations/associations.controller.ts
@@ -1,10 +1,15 @@
 import { Body, Controller, Delete, Get, Param, ParseArrayPipe, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { User } from 'src/users/user.entity';
+import { UsersService } from 'src/users/users.service';
 import { Association } from './association.entity';
 import { AssociationsService } from './associations.service';
 
 @Controller('associations')
 export class AssociationsController {
-    constructor(private service: AssociationsService){}
+    constructor(
+        private service: AssociationsService,
+        private users: UsersService
+        ){}
 
     @Get()
     getAll(): Association[] {
@@ -29,5 +34,10 @@ export class AssociationsController {
     @Delete(':id')
     delete(@Param('id', ParseIntPipe) id: number): boolean {
         return this.service.delete(id)
+    }
+
+    @Get(':id/members')
+    getMembers(@Param('id', ParseIntPipe) id: number): User[] {
+        return this.service.findById(id).idUsers.map(u => this.users.findUser(u))
     }
 }
--- fr-administration/src/associations/associations.module.ts
+++ fr-administration/src/associations/associations.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AssociationsService } from './associations.service';
 import { AssociationsController } from './associations.controller';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   providers: [AssociationsService],
-  controllers: [AssociationsController]
+  controllers: [AssociationsController],
+  imports: [UsersModule]
 })
 export class AssociationsModule {}
