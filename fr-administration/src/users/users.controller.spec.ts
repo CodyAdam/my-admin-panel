c963b673d50b24624b7e8e21f11baf5813c58016
Fgdou <fabigoardou@gmail.com>
2022-10-25 10:24:23 +0200

    test users.controller

--- fr-administration/src/users/users.controller.spec.ts
+++ fr-administration/src/users/users.controller.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+export type MockType<T> = {
+  [P in keyof T]?: jest.Mock<{}>;
+}
+
+export const respositoryMockFactory: () => MockType<Repository<any>> = jest.fn(() => ({
+  findOne: jest.fn(entity => entity),
+}))
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useFactory: respositoryMockFactory
+        }
+      ]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
