df3512353c3a58c283359335d9cbd7c441cb058f
Fgdou <fabigoardou@gmail.com>
2022-10-25 16:16:42 +0200

    FINISH asso controller test

--- fr-administration/src/associations/associations.controller.spec.ts
+++ fr-administration/src/associations/associations.controller.spec.ts
@@ -4,7 +4,7 @@ import { User } from '../users/user.entity';
 import { UsersService } from '../users/users.service';
 import { Repository } from 'typeorm';
 import { Association } from './association.entity';
-import { AssociationsController } from './associations.controller';
+import { AssociationsController, AssociationCreate, AssociationUpdate } from './associations.controller';
 import { AssociationsService } from './associations.service';
 
 export type MockType<T> = {
@@ -19,6 +19,8 @@ describe('AssociationsController', () => {
   let controller: AssociationsController;
   let service: AssociationsService
   let userService: UsersService
+
+  let users: Promise<User[]>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -37,9 +39,17 @@ describe('AssociationsController', () => {
       ]
     }).compile();
 
+    users = Promise.all([{
+      id: 0,
+      firstname: 'John',
+      lastname: 'Doe',
+      age: 23
+    }])
     service = module.get<AssociationsService>(AssociationsService)
     userService = module.get<UsersService>(UsersService)
     controller = module.get<AssociationsController>(AssociationsController);
+
+    jest.spyOn(userService, 'findUser').mockImplementation(async () => (await users)[0])
   });
 
   it('should be defined', () => {
@@ -66,12 +76,6 @@ describe('AssociationsController', () => {
   })
   describe('getOne', () => {
     it('should return one association', async () => {
-      let users: Promise<User[]> = Promise.all([{
-        id: 0,
-        firstname: 'John',
-        lastname: 'Doe',
-        age: 23
-      }])
       let expected: Promise<Association> = Promise.resolve({
         id: 0,
         name: 'ADAPEI',
@@ -80,6 +84,45 @@ describe('AssociationsController', () => {
 
       jest.spyOn(service, 'findById').mockImplementation(() => expected)
       expect(await controller.getOne(0)).toBe(await expected)
+    })
+  })
+  describe('create', () => {
+    it('should return the new association', async () => {
+      let assoCreate: AssociationCreate = {
+        name: 'ADAPEI',
+        idUsers: [0]
+      }
+      let expected: Promise<Association> = Promise.resolve({
+        id: 0,
+        name: assoCreate.name,
+        idUsers: users
+      })
+
+      jest.spyOn(service, 'create').mockImplementation(() => expected)
+      expect(await controller.create(assoCreate)).toBe(await expected)
+    })
+  })
+  describe('update', () => {
+    it('should return the updated version', async () => {
+      let assoUpdate: AssociationUpdate = {
+        name: 'ADAPEI22'
+      }
+      let expected: Promise<Association> = Promise.resolve({
+        id: 0,
+        name: assoUpdate.name,
+        idUsers: users
+      })
+
+      jest.spyOn(service, 'update').mockImplementation(() => expected)
+      expect(await controller.update(0, assoUpdate)).toBe(await expected)
+    })
+  })
+  describe('delete', () => {
+    it('should return true on deletion', async () => {
+      let id = 0;
+
+      jest.spyOn(service, 'delete').mockImplementation(async () => true)
+      expect(await controller.delete(id)).toBe(true)
     })
   })
 });
