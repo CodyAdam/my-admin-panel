5db774073017b9edaeb55fedbf326d18c3312669
Fgdou <fabigoardou@gmail.com>
2022-10-26 09:16:13 +0200

    ADD helmet

--- fr-administration/src/main.ts
+++ fr-administration/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import helmet from 'helmet'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
     .build()
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
+
+  app.use(helmet())
 
   await app.listen(3000);
 }
