ef8c2b1e5c26b4822fb7d0beb900393310d90d9d
CodyAdam <cody.ad4@gmail.com>
2022-11-11 17:40:22 +0100

    feat: add logout and navbar

--- fr-administration-front/src/app/app.component.ts
+++ fr-administration-front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TokenStorageService } from './services/token-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fr-administration-front';
+
+  constructor(private tokenStorage: TokenStorageService) {}
+
+  isLogged() {
+    return this.tokenStorage.isLogged();
+  }
+
+  handleLogout() {
+    this.tokenStorage.clear();
+    window.location.reload();
+
+  }
 }
