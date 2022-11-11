50d64083bd2c3399b7bb79f4d1c86bb684df4e40
CodyAdam <cody.ad4@gmail.com>
2022-11-11 15:10:28 +0100

    feat: add routes

---
+++ fr-administration-front/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { UsersListComponent } from './users-list/users-list.component';
+
+const routes: Routes = [
+  { path: 'users', component: UsersListComponent },
+  { path: '', component: LoginComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
