9dc2d5265e74f523be30ae0187722cb6359eb904
CodyAdam <cody.ad4@gmail.com>
2022-11-11 17:39:44 +0100

    feat: add 404 page

--- fr-administration-front/src/app/app-routing.module.ts
+++ fr-administration-front/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
 import { LoginComponent } from './login/login.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { UsersListComponent } from './users-list/users-list.component';
 
 const routes: Routes = [
   { path: 'users', component: UsersListComponent, canActivate: [AuthGuard] },
   { path: '', component: LoginComponent },
+  { path: '**', pathMatch: 'full', component: PagenotfoundComponent },
 ];
 
 @NgModule({
