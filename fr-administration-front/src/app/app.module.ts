4828c12badcb07c6c42fcd071942867cd5e85dab
CodyAdam <cody.ad4@gmail.com>
2022-11-11 15:10:23 +0100

    feat: import modules Materials

---
+++ fr-administration-front/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [AppComponent, UsersListComponent, LoginComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
