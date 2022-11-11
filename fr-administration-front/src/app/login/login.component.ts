ba43cce221de2a5bcef289e2cdc9eccefc96e73c
CodyAdam <cody.ad4@gmail.com>
2022-11-11 16:05:04 +0100

    feat: add login to back http

--- fr-administration-front/src/app/login/login.component.ts
+++ fr-administration-front/src/app/login/login.component.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiHelperService } from '../services/api-helper.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnInit {
+  public username: string | null = null;
+  public password: string | null = null;
+  constructor(private api: ApiHelperService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  login(): void {
+    console.log('login', this.username, this.password);
+    const [username, password] = [this.username, this.password];
+    this.api
+      .post({ endpoint: '/auth/login', data: { username, password } })
+      .then((response) => console.log(response));
   }
 
+  handleChangeUsername(event: any): void {
+    this.username = (event.target as HTMLInputElement).value;
+  }
+
+  handleChangePassword(event: any): void {
+    this.password = (event.target as HTMLInputElement).value;
+  }
 }
