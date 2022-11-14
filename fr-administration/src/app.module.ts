b303e0d35e9df066296a43824318d382b2c23540
Fgdou <fabigoardou@gmail.com>
2022-11-14 15:42:42 +0100

    Use postgres database

--- fr-administration/src/app.module.ts
+++ fr-administration/src/app.module.ts
@@ -15,8 +15,12 @@ import { Minute } from './minutes/minute.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'mydatabase.db',
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [
         User,
         Association,
