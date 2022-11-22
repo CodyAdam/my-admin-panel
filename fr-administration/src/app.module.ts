4bbae57f77ea5c4975ae57febbed8a721d41a8c4
CodyAdam <cody.ad4@gmail.com>
2022-11-22 14:40:19 +0100

    feat: detach db finished

--- fr-administration/src/app.module.ts
+++ fr-administration/src/app.module.ts
@@ -16,11 +16,11 @@ import { Minute } from './minutes/minute.entity';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'example',
+      database: 'postgres',
       entities: [User, Association, Role, Minute],
       synchronize: true,
     }),
