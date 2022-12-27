d117054f58c6cef2d1174ff4be60d68b24198c42
CodyAdam <cody.ad4@gmail.com>
2022-12-28 00:31:14 +0100

    feat: add register

--- fr-administration-front/src/app/login/login.component.ts
+++ fr-administration-front/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiHelperService } from '../services/api-helper.service';
 import { TokenStorageService } from '../services/token-storage.service';
@@ -11,7 +11,12 @@ import { TokenStorageService } from '../services/token-storage.service';
 export class LoginComponent implements OnInit {
   public username: string | null = null;
   public password: string | null = null;
+  public registerUsername: string | null = null;
+  public registerPassword: string | null = null;
   public state: 'loading' | 'error' | 'success' | 'idle' = 'idle';
+  @ViewChild('loginForm') loginForm: ElementRef<HTMLFormElement> | null = null;
+  @ViewChild('registerForm') registerForm: ElementRef<HTMLFormElement> | null =
+    null;
   constructor(
     private api: ApiHelperService,
     private token: TokenStorageService,
@@ -21,11 +26,34 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     if (this.token.isLogged()) this.state = 'success';
   }
+
   login() {
+    if (!this.loginForm || !this.loginForm.nativeElement.checkValidity())
+      return;
     this.state = 'loading';
+
     const [username, password] = [this.username, this.password];
     this.api
       .post({ endpoint: '/auth/login', data: { username, password } })
+      .then((response) => {
+        if (response.access_token) {
+          this.token.save(response.access_token);
+          this.state = 'success';
+          this.router.navigateByUrl('/users');
+        } else this.state = 'error';
+      })
+      .catch((error) => {
+        this.state = 'error';
+      });
+  }
+
+  register() {
+    if (!this.registerForm || !this.registerForm.nativeElement.checkValidity())
+      return;
+    this.state = 'loading';
+    const [username, password] = [this.registerUsername, this.registerPassword];
+    this.api
+      .post({ endpoint: '/auth/register', data: { username, password } })
       .then((response) => {
         if (response.access_token) {
           this.token.save(response.access_token);
@@ -46,5 +74,17 @@ export class LoginComponent implements OnInit {
   handleChangePassword(event: any): void {
     this.password = (event.target as HTMLInputElement).value;
     this.state = 'idle';
+  }
+  handleChangeRegisterUsername(event: any): void {
+    this.registerUsername = (event.target as HTMLInputElement).value;
+    this.state = 'idle';
+  }
+  handleChangeRegisterPassword(event: any): void {
+    this.registerPassword = (event.target as HTMLInputElement).value;
+    this.state = 'idle';
+  }
+
+  submit(event: any): void {
+    event.preventDefault();
   }
 }
