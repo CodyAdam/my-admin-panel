bd8c7500c1f0196bbd3fc45dadcd0c58003bd8e1
CodyAdam <cody.ad4@gmail.com>
2023-01-07 19:01:43 +0100

    feat: auth add id to response

--- fr-administration/src/auth/auth.service.ts
+++ fr-administration/src/auth/auth.service.ts
@@ -26,6 +26,7 @@ export class AuthService {
     const payload = { email: user.email, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
+      id: user.id,
     };
   }
 }
