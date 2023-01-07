695f61644f1a80d4d25685e7b9dace1628d3044c
CodyAdam <cody.ad4@gmail.com>
2023-01-07 19:04:29 +0100

    feat: store id of logged user

--- fr-administration-front/src/app/services/token-storage.service.ts
+++ fr-administration-front/src/app/services/token-storage.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 const TOKEN_KEY = 'token';
-const USERNAME_KEY = 'username';
+const USER_ID = 'userId';
 const IS_LOGGED_IN = 'isLoggedIn';
-const IS_LOGGED = 'true';
 @Injectable({
   providedIn: 'root',
 })
@@ -10,17 +9,24 @@ export class TokenStorageService {
   public clear(): void {
     localStorage.clear();
   }
-  public save(token: string): void {
+  public save(token: string, id: number): void {
     localStorage.removeItem(TOKEN_KEY);
-    localStorage.removeItem(USERNAME_KEY);
     localStorage.removeItem(IS_LOGGED_IN);
+    localStorage.removeItem(USER_ID);
     localStorage.setItem(TOKEN_KEY, token);
-    localStorage.setItem(IS_LOGGED_IN, IS_LOGGED);
+    localStorage.setItem(USER_ID, id.toString());
+    localStorage.setItem(IS_LOGGED_IN, 'true');
   }
   public getToken(): string {
     const token = localStorage.getItem(TOKEN_KEY);
     return token === null ? '' : token;
   }
+
+  public getUserId(): number {
+    const id = localStorage.getItem(USER_ID);
+    return id === null ? NaN : Number(id);
+  }
+
   public isLogged(): boolean {
     return Boolean(localStorage.getItem(IS_LOGGED_IN));
   }
