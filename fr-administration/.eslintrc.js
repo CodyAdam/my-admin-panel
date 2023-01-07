7ccf2d0bb244334f35851c5a3f16ad75779688d6
CodyAdam <cody.ad4@gmail.com>
2023-01-07 16:42:41 +0100

    chore: eslint 2 spaces indent

--- fr-administration/.eslintrc.js
+++ fr-administration/.eslintrc.js
@@ -21,5 +21,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    // number of tabs spaces for indentation 2
+    '@typescript-eslint/indent': ['error', 2],
   },
 };
